3bbc1cf4ebe98689c7207e48ca779a2ae01ccefa
LeeSh-213 <hasanghyun66@naver.com>
2025-07-18 15:11:50 +0900

    [level 1] Title: 둘만의 암호, Time: 0.03 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+string solution(string s, string skip, int index) {
+    string answer = "";
+    unordered_set<char> skipSet(skip.begin(), skip.end());
+    
+    for (char c : s )
+    {
+        int count = 0; 
+        char current = c;
+        
+        while (count < index)
+        {
+            current++;
+            if (current > 'z') current = 'a';
+            if (skipSet.find(current) != skipSet.end()) continue;
+            count++;
+        }
+        
+        answer += current;
+    }
+    
+    
+    return answer;
+}
